330d3429f2459077ba3c69ee66451dc7ad3b7ddf
gray509 <ddorian263@gmail.com>
2025-05-31 14:03:39 -0400

    fix initialzation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from constants import *
 import pygame
 def main():
+    pygame.init()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return
-    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while(True):
         screen.fill((0,0,0))
