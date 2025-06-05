ab10c1b9afe4ec356c7b1f7645683580d3495cd4
dorian <ddorian263@gmail.com>
2025-06-05 12:08:54 -0400

    player rotate and moves

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 def main():
     pygame.init()
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    fps = pygame.time.Clock()
+    dt  = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         screen.fill((0,0,0))
+        
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = fps.tick(60) / 1000
     
 
 if __name__ == "__main__":
