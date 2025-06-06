74ea3800f133d4379c945031e05bf1f1dd9e7eda
dorian <ddorian263@gmail.com>
2025-06-05 20:11:58 -0400

    asteroids move on the screen

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import pygame
 from constants import *
 from player import Player
-
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     fps = pygame.time.Clock()
     dt  = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    asteroids_fields = AsteroidField()
     
     while(True):
         for event in pygame.event.get():
@@ -17,8 +27,10 @@ def main():
         
         screen.fill((0,0,0))
         
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        
         pygame.display.flip()
         
         dt = fps.tick(60) / 1000
